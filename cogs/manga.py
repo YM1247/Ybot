9affda89ced4c822596ac9835d4de2f0365ae896
YM1247 <liam960124@gmail.com>
2023-02-02 13:09:59 +0800

    update manga

--- cogs/manga.py
+++ cogs/manga.py
@@ -1,20 +1,15 @@
-import discord, webbrowser
+import discord
 from discord.ext import commands
+from discord.ui import Button, View
 from core.classes import cogExtension
 
 class manga(cogExtension):
-    @commands.slash_command(name = '番號', description = '輸入番號，然後瘋狂射精!')
-    async def cum(self, ctx, code, type):
-        if type == 'e':
-            url = f'https://www.wnacg.org/photos-index-aid-{code}'
-        elif type == 'j':
-            url = f'https://18comic.org/album/{code}'
-        else:
-            url = f'https://nhentai.net/g/{code}'
-        
-
-        webbrowser.open(url)
-        await ctx.respond(f'已成功開啟車號{code}!')
+    @commands.slash_command(name = 'cum', description = '輸入番號，然後瘋狂射精!')
+    async def cum(self, ctx, code):
+        url = f'https://nhentai.net/g/{code}'
+        button = Button(label = f'點此前往: {code}', style = discord.ButtonStyle.green, url = url)   
+        view = View(button)
+        await ctx.respond(view = view)
 
 def setup(bot):
     bot.add_cog(manga(bot))
